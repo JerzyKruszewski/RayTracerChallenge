39e7b0d2c0ec777cc4696d654dc721b80550d39a
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2021-02-20 12:45:41 +0100

    Added color operation with unit tests

--- RayTracer.MathLibrary.Tests/ColorTests.cs
+++ RayTracer.MathLibrary.Tests/ColorTests.cs
@@ -27,5 +27,127 @@ namespace RayTracer.MathLibrary.Tests
         {
             Assert.AreEqual(expected, new Color(red, green, blue).Blue);
         }
+
+        [Test]
+        [TestCase(1f, -1f, 0f, 0f, 0f, 1f, 1f, -1f, -1f)]
+        [TestCase(1f, 1f, 6f, 3f, -2f, 5f, -2f, 3f, 1f)]
+        [TestCase(1.6f, 0.7f, 1.0f, 0.9f, 0.6f, 0.75f, 0.7f, 0.1f, 0.25f)]
+        public void AdditionOperator_WhenCalledWithTwoColors_ReturnColorWithAddedColorValues(float expectedX, float expectedY, float expectedZ,
+                                                                                             float arg1X, float arg1Y, float arg1Z,
+                                                                                             float arg2X, float arg2Y, float arg2Z)
+        {
+            Color color1 = new Color()
+            {
+                Red = arg1X,
+                Green = arg1Y,
+                Blue = arg1Z
+            };
+
+            Color color2 = new Color()
+            {
+                Red = arg2X,
+                Green = arg2Y,
+                Blue = arg2Z
+            };
+
+            Color expected = new Color()
+            {
+                Red = expectedX,
+                Green = expectedY,
+                Blue = expectedZ
+            };
+
+            Assert.IsTrue(Color.AreColorsEqual(expected, color1 + color2));
+        }
+
+        [Test]
+        [TestCase(-1f, 1f, 2f, 0f, 0f, 1f, 1f, -1f, -1f)]
+        [TestCase(-2f, -4f, -6f, 3f, 2f, 1f, 5f, 6f, 7f)]
+        [TestCase(0.2f, 0.5f, 0.5f, 0.9f, 0.6f, 0.75f, 0.7f, 0.1f, 0.25f)]
+        public void SubtractionOperator_WhenCalledWithTwoColors_ReturnColorWithSubtractedColorValues(float expectedX, float expectedY, float expectedZ,
+                                                                                                     float arg1X, float arg1Y, float arg1Z,
+                                                                                                     float arg2X, float arg2Y, float arg2Z)
+        {
+            Color color1 = new Color()
+            {
+                Red = arg1X,
+                Green = arg1Y,
+                Blue = arg1Z
+            };
+
+            Color color2 = new Color()
+            {
+                Red = arg2X,
+                Green = arg2Y,
+                Blue = arg2Z
+            };
+
+            Color expected = new Color()
+            {
+                Red = expectedX,
+                Green = expectedY,
+                Blue = expectedZ
+            };
+
+            Assert.IsTrue(Color.AreColorsEqual(expected, color1 - color2));
+        }
+
+        [Test]
+        [TestCase(0f, 0f, -1f, 0f, 0f, 1f, 1f, -1f, -1f)]
+        [TestCase(15f, 12f, 7f, 3f, 2f, 1f, 5f, 6f, 7f)]
+        [TestCase(0.9f, 0.2f, 0.04f, 1f, 0.2f, 0.4f, 0.9f, 1f, 0.1f)]
+        public void MultiplyOperator_WhenCalledWithTwoColors_ReturnColorWithMultipliedColorValues(float expectedX, float expectedY, float expectedZ,
+                                                                                                  float arg1X, float arg1Y, float arg1Z,
+                                                                                                  float arg2X, float arg2Y, float arg2Z)
+        {
+            Color color1 = new Color()
+            {
+                Red = arg1X,
+                Green = arg1Y,
+                Blue = arg1Z
+            };
+
+            Color color2 = new Color()
+            {
+                Red = arg2X,
+                Green = arg2Y,
+                Blue = arg2Z
+            };
+
+            Color expected = new Color()
+            {
+                Red = expectedX,
+                Green = expectedY,
+                Blue = expectedZ
+            };
+
+            Assert.IsTrue(Color.AreColorsEqual(expected, color1 * color2));
+        }
+
+        [Test]
+        [TestCase(2f, 2f, 2f, 1f, 1f, 1f, 2f)]
+        [TestCase(3f, 3f, 3f, 2f, 2f, 2f, 1.5f)]
+        [TestCase(3.5f, -7f, 10.5f, 1f, -2f, 3f, 3.5f)]
+        [TestCase(0.5f, -1f, 1.5f, 1f, -2f, 3f, 0.5f)]
+        [TestCase(0.4f, 0.6f, 0.8f, 0.2f, 0.3f, 0.4f, 2f)]
+        public void MultiplyOperator_WhenCalledWithColorAndScalar_ReturnColorWithMultipliedColorValues(float expectedX, float expectedY, float expectedZ,
+                                                                                                       float arg1X, float arg1Y, float arg1Z, float scalar)
+        {
+            Color color = new Color()
+            {
+                Red = arg1X,
+                Green = arg1Y,
+                Blue = arg1Z
+            };
+
+            Color expected = new Color()
+            {
+                Red = expectedX,
+                Green = expectedY,
+                Blue = expectedZ
+            };
+
+            Assert.IsTrue(Color.AreColorsEqual(expected, color * scalar));
+        }
     }
 }
--- RayTracer.MathLibrary/Color.cs
+++ RayTracer.MathLibrary/Color.cs
@@ -23,5 +23,48 @@ namespace RayTracer.MathLibrary
             Green = green;
             Blue = blue;
         }
+
+        public static Color operator +(Color color1, Color color2)
+        {
+            color1.Red += color2.Red;
+            color1.Green += color2.Green;
+            color1.Blue += color2.Blue;
+
+            return color1;
+        }
+
+        public static Color operator -(Color color1, Color color2)
+        {
+            color1.Red -= color2.Red;
+            color1.Green -= color2.Green;
+            color1.Blue -= color2.Blue;
+
+            return color1;
+        }
+
+        public static Color operator *(Color color1, Color color2)
+        {
+            color1.Red *= color2.Red;
+            color1.Green *= color2.Green;
+            color1.Blue *= color2.Blue;
+
+            return color1;
+        }
+
+        public static Color operator *(Color color, float scalar)
+        {
+            color.Red *= scalar;
+            color.Green *= scalar;
+            color.Blue *= scalar;
+
+            return color;
+        }
+
+        public static bool AreColorsEqual(Color color1, Color color2, float epsilon = 0.00001f)
+        {
+            return MathHelper.AreNumbersEqual(color1.Red, color2.Red, epsilon)
+                && MathHelper.AreNumbersEqual(color1.Green, color2.Green, epsilon)
+                && MathHelper.AreNumbersEqual(color1.Blue, color2.Blue, epsilon);
+        }
     }
 }
