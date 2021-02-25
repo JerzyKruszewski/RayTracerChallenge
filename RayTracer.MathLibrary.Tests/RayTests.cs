75fe1ce174b32cec9ad34280fb66122651b7dcad
Jerzy Kruszewski <jerzyj.kruszewski@gmail.com>
2021-02-25 16:49:13 +0100

    Added rays

---
+++ RayTracer.MathLibrary.Tests/RayTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace RayTracer.MathLibrary.Tests
+{
+    public class RayTests
+    {
+        [Test]
+        [TestCase(2, 3, 4,
+                  2, 3, 4,
+                  1, 0, 0,
+                  0)]
+        [TestCase(3, 3, 4,
+                  2, 3, 4,
+                  1, 0, 0,
+                  1)]
+        [TestCase(1, 3, 4,
+                  2, 3, 4,
+                  1, 0, 0,
+                  -1)]
+        [TestCase(4.5, 3, 4,
+                  2, 3, 4,
+                  1, 0, 0,
+                  2.5)]
+        public void Position_WhenCalled_ReturnPointRepresentingPositionOfRay(double expectedX, double expectedY, double expectedZ,
+                                                                             double originX, double originY, double originZ,
+                                                                             double directionX, double directionY, double directionZ,
+                                                                             double time)
+        {
+            Point3D expected = new Point3D(expectedX, expectedY, expectedZ);
+
+            Ray ray = new Ray(new Point3D(originX, originY, originZ), new Vector3(directionX, directionY, directionZ));
+
+            Assert.IsTrue(Utilities.AreObjectEquals(expected, ray.Position(time)));
+        }
+    }
+}
---
+++ RayTracer.MathLibrary/Ray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RayTracer.MathLibrary
+{
+    public class Ray
+    {
+        public readonly Point3D _origin;
+
+        public readonly Vector3 _direction;
+
+        public Ray()
+        {
+
+        }
+
+        public Ray(Point3D origin, Vector3 direction)
+        {
+            _origin = origin;
+            _direction = direction;
+        }
+
+        public Point3D Position(double time)
+        {
+            return _origin + _direction * time;
+        }
+    }
+}
